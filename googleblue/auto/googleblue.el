0e32b08c03e4c78e26d7d7a27d12360ca5abe998
WisdomFusion <WisdomFusion@gmail.com>
2018-05-27 14:05:07 +0800

    init googleblue template

---
+++ googleblue/auto/googleblue.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "googleblue"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "english")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "babel"
+    "graphicx"
+    "hyperref"
+    "braket"
+    "listings")
+   (LaTeX-add-listings-lstdefinestyles
+    "customsql"))
+ :latex)
+
